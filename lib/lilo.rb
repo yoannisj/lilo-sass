bfe7463706e70adde57db64c7c0c709f40e345ed
Yoannis Jamar <yoannis.j@gmail.com>
2016-10-28 15:49:46 +0200

    version bump

--- lib/lilo.rb
+++ lib/lilo.rb
@@ -22,6 +22,6 @@ end
 # Version is a number. If a version contains alphas, it will be created as a prerelease version
 # Date is in the form of YYYY-MM-DD
 module Lilo
-    VERSION = "0.1.1"
-    DATE = "2016-10-18"
+    VERSION = "0.1.2"
+    DATE = "2016-10-28"
 end
